3283d9f8f8b80e3d3f6aca4fb166b60684a972dd
J.S. Easley <easleyjs@gmail.com>
2023-11-19 22:34:43 -0800

    Organizes App.js dependencies

--- App.js
+++ App.js
@@ -1,17 +1,21 @@
 import React, { useState } from 'react';
 import { Provider } from 'react-redux';
-import Header from './src/components/header';
+import store from './src/redux';
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
+import { NativeBaseProvider, Box } from 'native-base';
+
+import { Text, TouchableOpacity, SafeAreaView } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Header from './src/components/header';
 import Login from './src/screens/Login';
 import Signup from './src/screens/Signup';
 import UserHome from './src/screens/UserHome';
 import Footer from './src/components/footer';
 import AuctionRoomUser from './src/screens/AuctionRoomUser';
-import store from './src/redux';
-import { NativeBaseProvider, Box } from 'native-base';
-import { Text, TouchableOpacity, SafeAreaView } from 'react-native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 // export default function App() {
 //     const [role, setRole] = useState('user');
